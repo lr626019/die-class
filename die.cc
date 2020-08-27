28ead7bee49a7fc86d5f97ab4d007d2d75614e7e
Logan Richards <lrichards0217@comcast.net>
2020-08-27 16:19:43 -0400

    Makef Commit

--- die.cc
+++ die.cc
@@ -1,50 +1,50 @@
-#​include​ ​<iostream>
-#​include​ ​<fstream>
-​#include​ ​<cstdlib>
-#​include​ ​"die.h"
-using​ ​namespace​ ​std​;
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "die.h"
+using namespace std; 
 
 //Constructors
-Die::Die​(){ ​//default 6 sides
-    numSides = ​6​;
-    ​srand​(​time​(​NULL​));
-    faceValue = ​rand​() % numSides + ​1​;
+Die::Die(){ //default 6 sides
+    numSides = 6;
+    srand(time(NULL));
+    faceValue = rand() % numSides + 1;
 }
 
-Die::Die​(​int​ newNumSides){
-    if​ (newNumSides > ​1​) {
+Die::Die(int newNumSides){
+    if (newNumSides>1) {
         numSides = newNumSides;
     }
-
-    else​ {
-        numSides = ​6​;
-        srand​(​time​(​NULL​));
-        faceValue = ​rand​() % numSides + ​1​;
+    else
+    {
+        numSides=6;
+        srand(time(NULL));
+        faceValue=rand()%numSides+1;
     }
 }
 
 //setters
-void​ ​Die::setNumSides​(​int​ newNumSides){
-    if​ (newNumSides > ​1​) {
-        numSides = newNumSides;
+void Die::setNumSides(int newNumSides){
+    if(newNumSides>1){
+        numSides=newNumSides;
     }
 }
 
 //getters
-int​ ​Die::getFaceValue​(){
-    return​ faceValue;
+int Die::getFaceValue(){
+    return faceValue;
 }
-int Die::getNumSides​(){
-    return​ numSides;
+int Die::getNumSides(){
+    return numSides;
 }
 
 //helper functions
-int​ ​Die::roll​(){
-    faceValue = ​rand​() % numSides + ​1​;
-    return​ faceValue;
+int Die::roll(){
+    faceValue=rand()%numSides+1;
+    return faceValue;
 }
 
-ostream& ​operator​ <<(ostream &out, ​const​ Die &d){
-    out << d.faceValue;
-    return​ out;
+ostream& operator <<(ostream &out, const Die &d){
+    out <<d.faceValue;
+    return out;
 }
